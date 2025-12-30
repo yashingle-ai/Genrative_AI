f9f2755de20eef743121a96da2af1aed25b855d6
Yash Ingle <yash.ingle003@gmail.com>
2025-12-31 00:50:11 +0530

    Create multiple_tool_integration.py

---
+++ google ADK/tools/multiple_tool_integration.py
@@ -0,0 +1,27 @@
+from google.adk.tools.agent_tool import AgentTool
+from google.adk.agents import Agent
+from google.adk.tools import google_search
+from google.adk.code_executors import BuiltInCodeExecutor
+
+search_agent = Agent(
+    model='gemini-2.0-flash',
+    name='SearchAgent',
+    instruction="""
+    You're a specialist in Google Search
+    """,
+    tools=[google_search],
+)
+coding_agent = Agent(
+    model='gemini-2.0-flash',
+    name='CodeAgent',
+    instruction="""
+    You're a specialist in Code Execution
+    """,
+    code_executor=BuiltInCodeExecutor(),
+)
+root_agent = Agent(
+    name="RootAgent",
+    model="gemini-2.0-flash",
+    description="Root Agent",
+    tools=[AgentTool(agent=search_agent), AgentTool(agent=coding_agent)],
+)
